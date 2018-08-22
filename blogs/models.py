0bdec2d4269ca70b3063e394b27c923ce88e9be2
Doppon <zaxpkuraudo7777@gmail.com>
2018-08-22 23:56:05 +0700

    ちゃんとインテンドを揃える

--- blogs/models.py
+++ blogs/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Post(models.Model):
-	title = models.CharField(maxlength=255)
+	title = models.CharField(max_length=255)
 	text = models.TextField()
-    author = models.ForeignKey(
-        'auth.User',
-        on_delete=models.CASCADE,
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+	author = models.ForeignKey(
+		'auth.User',
+		on_delete=models.CASCADE,
+	)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
